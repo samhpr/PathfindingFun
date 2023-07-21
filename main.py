d29565551225ee8d7661fc802b6f21614400e80b
samhpr <samuel.hopper23@gmail.com>
2023-07-20 23:25:46 -0700

    more algorithm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # A* Pathfinding visualization tool made for fun & learning purposes
 # Currently following implementation and explanations by Tech With Tim to help create the core 
 # source: https://www.youtube.com/watch?v=JtiK0DOeI4A
+# more info: https://en.wikipedia.org/wiki/A*_search_algorithm
+
 
 import pygame
 from queue import PriorityQueue
@@ -56,7 +58,7 @@ class Node:
         return self.color == GREEN
     
     def reset(self):
-        self.color == WHITE
+        self.color = WHITE
 
     # to set everything
     def make_closed(self):
@@ -82,7 +84,21 @@ class Node:
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
     def update_neighbors(self, grid):
-        pass
+        self.neighbors = []
+        # checking if the row we are at is less than the total amount of rows - 1, else crash program because can not go down
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier(): # going DOWN a row (checking if can move down)
+            # then append the next row down
+            self.neighbors.append(grid[self.row + 1][self.col].is_barrier())
+
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier(): # going UP a row (checking if we can move up)
+            self.neighbors.append(grid[self.row - 1][self.col].is_barrier())
+
+        if self.row < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier(): # going RIGHT a row (checking if we can move right)
+            self.neighbors.append(grid[self.row][self.col + 1].is_barrier())
+
+        # greater than 0 because this is left of the screen, similar for going up as it is the top
+        if self.row > 0 and not grid[self.row][self.col - 1].is_barrier(): # going LEFT a row (checking if we can move left)
+            self.neighbors.append(grid[self.row][self.col - 1].is_barrier())
 
     # less than function to compare two Node objects together
     # similar to compareTo in Java
@@ -94,6 +110,41 @@ def h(p1, p2):
     x2, y2 = p2
     # return absolute distance to find the L distance
     return abs(x1 - x2) + abs(y1 - y2)
+
+def algorithm(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    # 'push' is same as 'put'
+    # we can use count to consider the tiebreakers (same F score)
+    # .put('fscore', 'number', 'current node')
+    open_set.put((0, count, start))
+    is_from = {}
+    # dictionary for the g score
+    g_score = {Node: float("inf") for row in grid for spot in row}
+    g_score[start] = 0
+    # f score is the heuristic because we want to estimate how far end node is from the start node and give that a score
+    f_score = {Node: float("inf") for row in grid for spot in row}
+    f_score[start] = h(start.get_pos(), end.get_pos())
+
+    # we want to make a set because the priority queue needs to be checked if there is something in it
+    # aka, it keeps track of the items in the priority queue, and able to check
+    open_set_hash = {start}
+
+    # run until open set is empty, considered every possible node and path does not exist
+    while not open_set.empty():
+        for event in pygame.event.get():
+            # feature to allow user to quit from while loop
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
+        # start at 2 because we want to skip past start and end nodes
+        current = open_set.get()[2]
+        # take whatever node from queue and ensure there are no duplicates by syncing with open set hash
+        open_set_hash.remove(current)
+
+        if current == end:
+            pass # make path
+
 
 def make_grid(rows, width):
     grid = []
@@ -140,6 +191,7 @@ def get_clicked_pos(pos, rows, width):
     col = x // gap
     
     return row, col
+    
 
 def main(win, width):
     ROWS = 50
@@ -165,11 +217,11 @@ def main(win, width):
                 pos = pygame.mouse.get_pos()
                 row, col = get_clicked_pos(pos, ROWS, width)
                 node = grid[row][col]
-                if not start:
+                if not start and node != end:
                     start = node
                     start.make_start()
 
-                elif not end:
+                elif not end and node != start:
                     end = node
                     end.make_end()
 
@@ -177,7 +229,29 @@ def main(win, width):
                     node.make_barrier()
             
             elif pygame.mouse.get_pressed()[2]: # right mouse button
-                pass
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                node = grid[row][col]
+                node.reset()
+
+                if node == start:
+                    start = None
+                
+                if node == end:
+                    end = None
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not started:
+                    for row in grid:
+                        for node in row:
+                            node.update_neighbors()
+
+                    # passing function that is equal to function call, Lamda is an anonymous function
+                    # for example, x = Lamda: print("hello")
+                    # calling x will call the print function
+                    # essentially, a function without a name, more info: 
+                    #           https://www.geeksforgeeks.org/python-lambda-anonymous-functions-filter-map-reduce/
+                    algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
 
     pygame.quit()
 
